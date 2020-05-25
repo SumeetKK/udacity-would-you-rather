f81b4199c6ef705fe80bc934b8da8759e5c94cb1
Sumeet Kumar <sumeet@evolvingtech.pk>
2020-05-25 12:10:31 +0500

    Converted Buttons to Nav Tabs on Homepage

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Container, Row, Col, Button} from 'reactstrap';
+import { Container, Row, Col, Nav, NavLink, NavItem, TabPane, TabContent} from 'reactstrap';
 import ListQuestion from './ListQuestion'
 
 class Home extends Component {
+
+    state = { activeTab: 'answered' }
+    
+      toggle(tab) {
+        if (this.state.activeTab !== tab) {
+          this.setState({ activeTab: tab });
+        }
+      }
+
   render() {
+      const answered = 'answered'
+      const unanswered = 'unanswered'
     return (
       <Container>
         <Row>
-            <Col md={{size: 4, offset: 2}}>
-                <Button color='primary' block>UnAnswered Questions</Button>
-            </Col>
-            <Col md='4'>
-                <Button outline color='primary' block>Answered Questions</Button>
+            <Col md={{size: 8, offset: 2}}>
+                <Nav tabs className='nav-justified'>
+                    <NavItem>
+                        <NavLink active={this.state.activeTab === answered } onClick={() => { this.toggle(answered); }}>
+                            Answered
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink active={this.state.activeTab === unanswered } onClick={() => { this.toggle(unanswered); }}>
+                            Unanswered
+                        </NavLink>
+                    </NavItem>
+                </Nav>
             </Col>
         </Row>
         <Row>
             <Col md={{size: 8, offset: 2}}>
-                <h3>Unanswered</h3>
-                    {this.props.unAnswered.map((id) => (
-                            <ListQuestion key={id} id={id} />
-                    ))}
-                <h3>Answered</h3>
-                    {this.props.answered.map((id) => (
-                        <ListQuestion key={id} id={id} />
-                    ))}
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId={answered}>
+                        { this.state.activeTab === answered
+                            && this.props.answered.map((id) => (
+                                <ListQuestion key={id} id={id} />
+                            ))}
+                    </TabPane>
+                    <TabPane tabId={unanswered}>
+                        { this.state.activeTab === unanswered
+                            && this.props.unAnswered.map((id) => (
+                                <ListQuestion key={id} id={id} />
+                            ))}
+                    </TabPane>
+                </TabContent>
             </Col>
         </Row>
       </Container>
